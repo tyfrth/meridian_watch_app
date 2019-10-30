e58d68fc59a1ca99ce6833fe3ad08dd168afc52b
tyfrth <tyfrith@gmail.com>
2019-10-30 13:31:05 -0600

    initial commit

---
+++ meridian_watch_app WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  meridian_watch_app WatchKit Extension
+//
+//  Created by Tyler Frith on 10/30/19.
+//  Copyright © 2019 sabotoothtigers. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ meridian_watch_app WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  meridian_watch_app WatchKit Extension
+//
+//  Created by Tyler Frith on 10/30/19.
+//  Copyright © 2019 sabotoothtigers. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
